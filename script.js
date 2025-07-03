5a71670039181d3601076e1d6665850d9b73bc91
roshan kumar <roshkr26@gmail.com>
2025-07-04 01:46:43 +0530

    roshan commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hours = document.querySelector(".hours");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
-
+// change 1
 (()=>{
      const date = new Date().toDateString();
      const obj = date.split(" ");
@@ -10,20 +10,30 @@ const seconds = document.querySelector(".seconds");
      document.querySelector(".day-number").textContent = obj[2];
      document.querySelector(".year").textContent = obj[3];
 })();
+
+// change 2
 function updateClock(){
      const time =new Date().toTimeString().split(" ")[0];
      let mins = time.split(":")[1];
      let sec = time.split(":")[2];
 
+
+     //change 3
      if(hrs > 12 ){
           document.querySelector(".ampm").textContent = 'PM';
      }
      else{
           document.querySelector(".ampm").textContent = 'AM';
      }
+
+     // change 4 
      hours.textContent = hrs;
      minutes.textContent = mins;
      seconds.textContent = sec;
 }
+
+// change 5
 updateClock();
 setInterval(updateClock,1000);
+
+// these are new changes 
